070527df92e6d7bfa26e556845769e162c334b45
luisferort <luis.fernandez.ortega@alumnos.upm.es>
2017-11-28 20:46:38 +0100

    test

---
+++ src/testsonarqube/TestSonarQube.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testsonarqube;
+
+/**
+ *
+ * @author Luis
+ */
+public class TestSonarQube {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int j = 0;
+        for (int i = 0; i <5 ; i++){
+            while (j < 10) {
+                System.out.println (j);
+                j++;
+            }
+            j=0;
+    }
+        
+    }
+    }
+    
+
